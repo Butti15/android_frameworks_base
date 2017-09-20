e6af3d470924a17cc1ed9a70a7d49a12d3475a73
maxwen <max.weninger@gmail.com>
2017-09-20 20:54:41 +0800

    1/2] base: unlink notification volume is back
     unused volume images -add new one for notification volumes maybe some day one
     will be able to create a vector drawable for this - I am not :)
    
    base: add vector for notification volume mute
    
    thanks to Marko Man
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4764,6 +4764,13 @@ public final class Settings {
         public static final String ENABLE_GAMING_MODE = "enable_gaming_mode";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
